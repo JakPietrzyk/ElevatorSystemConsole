e873f270ef95fbf4601b2c4fd41ed0b252923875
Spektra Kowalski <pietrzykj9@gmail.com>
2022-12-14 00:19:27 +0100

    async elevators

---
+++ ElevatorSystemConsole/Elevator.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ElevatorSystemConsole
+{
+    public class Elevator
+    {
+        public int currentFloor;
+        public int nextFloor;
+        public List<int> floorQueue;
+        public Elevator() 
+        {
+            currentFloor = 0;
+            nextFloor = 0;
+            floorQueue = new List<int>();
+        }
+        public void AddNewFloor(int floor)
+        {
+            floorQueue.Add(floor);
+        }
+        public void RemoveFloor(int floor) 
+        {
+            floorQueue.Remove(floor);
+        }
+        public async Task Run()
+        {
+            while (true)
+            {
+                while (floorQueue.Count > 0)
+                {
+                    nextFloor = floorQueue.First();
+                    floorQueue.Remove(floorQueue.First());
+                    Console.WriteLine("At {0} floor, next floor: {1}", currentFloor, nextFloor);
+                    while(currentFloor!=nextFloor)
+                    {
+                        if(currentFloor<nextFloor)
+                        {
+                            currentFloor++;
+                        }
+                        else
+                        {
+                            currentFloor--;
+                        }
+                        
+                        Thread.Sleep(2000);
+                        Console.WriteLine("At {0} floor, next floor: {1}", currentFloor, nextFloor);
+                    }
+                    Thread.Sleep(2000);
+                    currentFloor = nextFloor;
+                }
+                while(floorQueue.Count == 0 && currentFloor!=0)
+                {
+                    currentFloor--;
+                    nextFloor = 0;
+                    Console.WriteLine("At {0} floor, next floor: {1}", currentFloor, nextFloor);
+                    Thread.Sleep(2000);
+                }
+            }
+        }
+        public async Task AddFloorsConst()
+        {
+            while(true)
+            {
+                string uinput = Console.ReadLine();
+                int x = int.Parse(uinput);
+                floorQueue.Add(x);
+            }
+        }
+    }
+
+}
---
+++ ElevatorSystemConsole/Program.cs
@@ -0,0 +1,31 @@
+﻿using ElevatorSystemConsole;
+
+namespace Program
+{
+    public class Program
+    {
+        static void Main(string[] args) 
+        { 
+            Elevator elevator = new Elevator();
+            elevator.AddNewFloor(2);
+            //elevator.AddNewFloor(4);
+            //elevator.AddNewFloor(5);
+            //elevator.AddNewFloor(3);
+
+
+            var task = Task.Run(() =>
+            {
+                elevator.Run();
+            });
+            var task2 = Task.Run(() => { 
+                elevator.AddFloorsConst(); 
+            });
+
+            elevator.AddNewFloor(5);
+            task.Wait();
+            Console.ReadLine();
+            
+
+        }
+    }
+}
